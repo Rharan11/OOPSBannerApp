f800e726c727c400790b9590fe79ddf71f4713cd
Rharan11 <rh9278@srmist.edu.in>
2026-03-03 23:28:53 +0530

    UC6: Refactored banner using static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,62 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-                String.join("",
-                        "*********  ", "*********  ", "*********  ", "*********"),
-                String.join("",
-                        "*       *  ", "*       *  ", "*       *  ", "*        "),
-                String.join("",
-                        "*       *  ", "*       *  ", "*       *  ", "*        "),
-                String.join("",
-                        "*       *  ", "*       *  ", "*********  ", "*********"),
-                String.join("",
-                        "*       *  ", "*       *  ", "*          ", "        *"),
-                String.join("",
-                        "*       *  ", "*       *  ", "*          ", "        *"),
-                String.join("",
-                        "*********  ", "*********  ", "*          ", "*********")
-        };
+        String[] o = generateO();
+        String[] p = generateP();
+        String[] s = generateS();
 
-        for (String line : bannerLines) {
-            System.out.println(line);
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("    ",
+                    o[i],
+                    o[i],
+                    p[i],
+                    s[i]);
         }
 
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper method for O
+    public static String[] generateO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Helper method for P
+    public static String[] generateP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Helper method for S
+    public static String[] generateS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
